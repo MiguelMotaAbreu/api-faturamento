380a3627632cbc95b5d656fb1c6096533661bacc
Miguel Mota <miguelmota080806@gmail.com>
2025-07-08 18:53:18 -0300

    Adição de rotas POST para novos cadastros.
    - Criação de rotas POST baseadas em JSON
    
    - Prevenção de possíveis erros, como: duplicidade de dados

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 
+// Middleware para o Express entender JSON, garantindo mais segurança e compatibilidade para as rotas quanto ao recebimento de dados.
+app.use(express.json());
+
 // Estabelecendo conexão com o banco de dados.
 const PORT = 3000;
 const connection = mysql.createConnection({
@@ -12,7 +15,7 @@ const connection = mysql.createConnection({
   database: 'faturamento_db'
 });
 
-//Criando as Rotas
+//Criando as Rotas de requisição (GET)
 
 // Rota raiz, da 'página inicial'
 app.get('/', (req, res) => {
@@ -55,7 +58,7 @@ app.get('/pacientes/:id', (req, res) => {
   });
 });
 
-//Rota para TODOS os atendimentos
+//Rota para buscar TODOS os atendimentos
 app.get('/atendimentos', (req, res) => {
   const sqlQuery = `SELECT * FROM Atendimentos;`;
 
@@ -68,7 +71,7 @@ app.get('/atendimentos', (req, res) => {
   })
 })
 
-//Rota para os Atendimentos identificados por ID
+//Rota para buscar os Atendimentos identificados por ID
 app.get('/atendimentos/:id', (req, res) => {
   const atendimentosID = req.params.id;
   const sqlQuery = `SELECT * FROM Atendimentos WHERE id = ?;`;
@@ -83,6 +86,33 @@ app.get('/atendimentos/:id', (req, res) => {
     }
     return res.json(results[0]);
   })
+})
+
+//Criando rotas para inserção de novos pacientes (POST)
+app.post('/pacientes', (req, res) => {
+    //Aqui, define-se o esqueleto do json para novos POSTs no banco de dados.
+    const {nome_completo, data_nascimento, cpf} = req.body; //req.body cumpre essa função de guardar o esqueleto
+
+    //Validando se os dados necessários do novo paciente a ser cadastrado estão devidamente preenchidos
+    if (!nome_completo || !cpf){
+      return res.status(400).json({message: 'Nome completo e CPF são obrigatórios.'});
+    }
+    //Inserção SQL a ser executada dentro do banco de dados.
+    const sqlQuery = `INSERT INTO Pacientes (nome_completo, data_nascimento, cpf) VALUES (?, ?, ?);`;
+
+    connection.query(sqlQuery, [nome_completo, data_nascimento, cpf], (err, results) => {
+      if (err){
+        console.error("Erro ao criar paciente: ", err);
+        //A linha abaixo evita conflito de cadastros de pacientes pois retorna erro de duplicidade encontrada, caso seja verdadeiro.
+        if(err.code === 'ER_DUP_ENTRY'){
+            return res.status(409).json({message: 'Este CPF já está cadastrado.'})
+        }
+        //Abrangendo qualquer outro tipo de erro
+        return res.status(500).json({error: "Erro interno do servidor."})
+      }
+      const novoPacienteId = results.insertId; //Id em que foi cadastrado o novo paciente
+      res.status(201).json({ id: novoPacienteId, nome_completo, data_nascimento, cpf }); //Retornando o Id e o cadastro do paciente efetuado com sucesso nos parâmetros do banco de dados.
+    })
 })
 
 //Inicializando o servidor
